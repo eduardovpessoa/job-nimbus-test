a625f8cb8286ea12f2bbccdc622d3d99e4e136d1
Eduardo Viana Pessoa <idd_epessoa@uolinc.com>
2020-09-23 02:27:06 -0600

    Format and improve code legibility

--- problem-one.js
+++ problem-one.js
@@ -2,12 +2,24 @@ function verifyBrackets(text) {
     var openBracket = 0, closedBracket = 0;
     text.split("").forEach(element => {
         if (element == '{') {
-            if (openBracket < closedBracket) return false; else openBracket++;
+            if (openBracket < closedBracket) {
+                return false;
+            } else {
+                openBracket++;
+            }
         } else if (element == '}') {
-            if (openBracket < closedBracket) return false; else closedBracket++;
+            if (openBracket < closedBracket) {
+                return false;
+            } else {
+                closedBracket++;
+            }
         }
     });
-    if (openBracket == closedBracket) return true; else return false;
+    if (openBracket == closedBracket) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 module.exports = { verifyBrackets }
